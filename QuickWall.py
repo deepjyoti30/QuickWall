f9ece968648f58ab5da1838ba933e909b4d79b0b
kaushalpurohit <43072091+kaushalpurohit@users.noreply.github.com>
2019-09-13 19:28:09 +0530

    Add files via upload

--- QuickWall.py
+++ QuickWall.py
@@ -8,8 +8,8 @@ from QuickWall.SetPaper import SetPaper
 from QuickWall.utility import (is_nitrogen, clear_cache)
 from QuickWall.logger import Logger
 from QuickWall.setter import WallSetter
-
-
+from QuickWall import search
+from QuickWall import basic
 # Declare the logger
 logger = Logger("main")
 
@@ -28,67 +28,12 @@ def parse():
                         type=str, default="nitrogen")
     parser.add_argument('--dir', help="Directory to download the wallpapers",
                         type=str, default=None)
-
+    parser.add_argument('--search', help="Search",type=str)
     args = parser.parse_args()
 
     return args
 
 
-class Wall:
-    """
-    Class to do tasks like downloading the wallpaper.
-
-    URL list has 4 entries:
-
-    desc: Description of the image
-    name: Name of the user who uploaded the image
-    dw_link: Download link of the image
-    unique_id: ID to save the image
-    """
-    def __init__(self):
-        self._acces_key = "15bcea145de0b041ec8d3b16bf805e232c83cf52d569a06708aa51f33a4f14f4"
-        self._URL = 'https://api.unsplash.com/photos/?client_id={}&per_page=30'.format(self._acces_key)
-        self._URL_list = []
-
-    def _add_to_list(self, entity):
-        """
-        Extract the data from the passed entity and add it to
-        the list.
-        """
-
-        desc = entity['description']
-        if desc is None:
-            desc = entity['alt_description']
-
-        if desc is None:
-            desc = "Wallpaper"
-
-        name = entity['user']['name']
-        dw_link = entity['links']['download']
-        unique_id = entity['id']
-
-        self._URL_list.append({
-                                'dw_link': dw_link,
-                                'unique_id': unique_id,
-                                'desc': desc,
-                                'name': name
-                              })
-
-    def _get_paper(self):
-        """
-        Get a list of wallpaper using the access key.
-
-        Return a iterable list of direct download URL's.
-        """
-        response = requests.get(self._URL)
-        json_data = response.json()
-
-        for i in json_data:
-            self._add_to_list(i)
-
-    def get_list(self):
-        self._get_paper()
-        return self._URL_list
 
 
 def main():
@@ -98,12 +43,16 @@ def main():
     if args.clear_cache:
         clear_cache()
         exit(0)
-
+    
     # Get the wallpaper setter
     wall_setter = WallSetter(args.setter)
     setter = wall_setter.get_setter()
 
-    wall = Wall()
+    if args.search:
+        wall=search.Wall()
+    else:
+        wall=basic.Wall()
+
     logger.info("Gettting the wallpapers using Unsplash API...")
     paper_list = wall.get_list()
 
