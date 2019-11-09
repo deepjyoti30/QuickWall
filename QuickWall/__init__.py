1d2a79a23225839703e39c0b144f36cc24c2089c
deepjyoti30 <deep.barman30@gmail.com>
2019-11-09 19:08:34 +0530

    update: Added initial support for pywal.

--- QuickWall/__init__.py
+++ QuickWall/__init__.py
@@ -9,5 +9,6 @@ __all__ = ['download',
            'blacklist',
            'SetPaper',
            'search',
-           'wall'
+           'wall',
+           'wal'
           ]
---
+++ QuickWall/wal.py
@@ -0,0 +1,29 @@
+"""Set the theme extracted from the wallpaper."""
+
+from pywal import (
+    colors, export, sequences, theme, settings
+)
+import os
+from QuickWall.logger import Logger
+
+logger = Logger("wal")
+
+
+class Wal:
+    """Change the theme based on the passed wallpaper"""
+
+    def set(self, wallpaper):
+        logger.debug("{}".format(wallpaper))
+        self.colors_plain = colors.get(wallpaper)
+        sequences.send(self.colors_plain, to_send=True)
+        colors.palette()
+
+    def save(self):
+        export.every(self.colors_plain)
+
+    def restore(self):
+        self.colors_plain = theme.file(os.path.join(
+            settings.CACHE_DIR,
+            "colors.json"
+        ))
+        sequences.send(self.colors_plain, to_send=True)
