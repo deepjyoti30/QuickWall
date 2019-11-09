41d05004fe4cc5984114a497dc358fe52c354aec
deepjyoti30 <deep.barman30@gmail.com>
2019-11-09 22:41:54 +0530

    update: Added init automatic detection for wallpaper setter.

--- setup.py
+++ setup.py
@@ -11,5 +11,5 @@ setup(
     scripts=['scripts/QuickWall'],
     version='0.0.1-5',
     license='MIT',
-    install_requires=['requests', 'pywal', 'dbus'],
+    install_requires=['requests', 'pywal'],
 )
