7b4b9ecb92710cb5acc22fc02d610a843d49187e
Mazurel <mateusz.mazur@yahoo.com>
2019-11-17 23:16:19 +0100

    KDE - lockscreen and tweaks

--- QuickWall/kde.py
+++ QuickWall/kde.py
@@ -4,6 +4,10 @@ import dbus
 from os import path, system
 from shutil import copy
 
+from QuickWall.logger import Logger
+logger = Logger("nitrogen")
+
+
 def setwallpaper(filepath):
     """
     This script is taken from https://github.com/pashazz/ksetwallpaper/
@@ -24,6 +28,7 @@ def setwallpaper(filepath):
     plasma = dbus.Interface(bus.get_object('org.kde.plasmashell', '/PlasmaShell'), dbus_interface='org.kde.PlasmaShell')
     plasma.evaluateScript(jscript % filepath)
 
+
 def saveRestorableWallpaper():
     """
     Save current wallpaper as RestorableImage in kde wallpaper config 
@@ -37,6 +42,7 @@ def saveRestorableWallpaper():
     bus = dbus.SessionBus()
     plasma = dbus.Interface(bus.get_object('org.kde.plasmashell', '/PlasmaShell'), dbus_interface='org.kde.PlasmaShell')
     plasma.evaluateScript(jscript)
+
 
 def restoreWallpaper():
     """
@@ -58,6 +64,7 @@ def restoreWallpaper():
     plasma = dbus.Interface(bus.get_object('org.kde.plasmashell', '/PlasmaShell'), dbus_interface='org.kde.PlasmaShell')
     plasma.evaluateScript(jscript)
 
+
 class KDEsetpaper:
     def __init__(self):
         """Initialize KDE workflow"""
@@ -72,17 +79,21 @@ class KDEsetpaper:
         Permanetly set wallpaper.
 
         Creates restorableImage.jpg which allows kde to
-        restore image as it doesnt store images by itself
+        restore image as it doesnt store images by itself.
         """
         new_path = path.dirname(file_path) + "/restorableImage.jpg"
         copy(file_path, new_path)
         setwallpaper(new_path)
 
-        # Save to lock screen
-        # Based on https://github.com/RaitaroH/KDE-Terminal-Wallpaper-Changer
-        command = "kwriteconfig5 --file kscreenlockerrc --group Greeter --group Wallpaper --group org.kde.image --group General --key Image \"file://{}\"".format(new_path)
-        system(command)
+        ask = input("Do you want to set lockscreen wallpaper ? [Y]es [N]o ")
+        if (ask.lower() == "y"):
+            # Set lock screen as wallpaper
+            # Based on https://github.com/RaitaroH/KDE-Terminal-Wallpaper-Changer
+
+            command = "kwriteconfig5 --file kscreenlockerrc --group Greeter --group Wallpaper --group org.kde.image --group General --key Image \"file://{}\"".format(new_path)
+            system(command)
 
     def restore(self):
         """Restore wallpaper"""
+        logger.info("Restoring the last wallpaper...")
         restoreWallpaper()
