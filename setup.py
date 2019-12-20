99f5e59904231cf8a7080b5030ddc6be451a64d7
deepjyoti30 <deep.barman30@gmail.com>
2019-12-20 19:48:33 +0530

    dependencies: Added bs4 to requirements.

--- setup.py
+++ setup.py
@@ -11,5 +11,5 @@ setup(
     scripts=['scripts/QuickWall'],
     version='0.0.2',
     license='MIT',
-    install_requires=['requests', 'pywal', 'downloader-cli'],
+    install_requires=['requests', 'pywal', 'downloader-cli', 'beautifulsoup4'],
 )
